55e307979eb70c25189784ab86338f22860a9481
Brendan Keyport <brendan.keyport@gmail.com>
2022-09-23 14:17:13 -0700

    Initial Develop Branch

--- MMM-Multimonth.js
+++ MMM-Multimonth.js
@@ -1,125 +1,151 @@
 /* Magic Mirror Module: MMM-Multimonth
  *
- * v1.90 - August 2022 - Preparing for moment.js removal. 
  * By Brendan Keyport <brendan.keyport@gmail.com>
- * Major assistance and rewrite by Volker Wegert <github@volker-wegert.de> 
- * 
+ * Major assistance and rewrite by Volker Wegert <github@volker-wegert.de>
+ *
+ * See README.md for versioning and important info
+ *
  */
 
 Module.register("MMM-Multimonth", {
-	
-	// Default module config.
-	defaults: {
-		startMonth: -1, // Define when you start from current month
-		monthCount: 3, // Define How many months to display
-		monthsVertical: true, // Whether to arrange the months vertically (true) or horizontally (false).
-		repeatWeekdaysVertical: false, // Whether to repeat the week days in each month in vertical mode. Ignored in horizontal mode.
-		weekNumbers: false, // Whether to display the week numbers in front of each week.
-		highlightWeekend: false, // Highlight Saturday and Sunday
-	},
+  // Default module config.
+  defaults: {
+    startMonth: -1, // Define when you start from current month
+    monthCount: 3, // Define How many months to display
+    monthsVertical: true, // Whether to arrange the months vertically (true) or horizontally (false).
+    repeatWeekdaysVertical: false, // Whether to repeat the week days in each month in vertical mode. Ignored in horizontal mode.
+    weekNumbers: false, // Whether to display the week numbers in front of each week.
+    highlightWeekend: false // Highlight Saturday and Sunday
+  },
 
-	// CSS Add
-	getStyles: function() {
-		return [this.data.path + "/MMM-Multimonth.css"];
-	},
+  // CSS Add
+  getStyles: function () {
+    return [this.data.path + "/MMM-Multimonth.css"];
+  },
 
-	// Update at midnight
-	start: function (){
-		 var timer = setInterval(()=>{
+  // Update at midnight
+  start: function () {
+    date = new Date();
+    month = date.getMonth();
+    day = date.getDate();
+    nextday = day + 1;
+    year = date.getFullYear();
+    reset = new Date(year, month, nextday, 0, 0, 0, 0).getTime()-date.getTime();
+   var timer = setInterval(()=>{
 		 this.updateDom()
-		}, moment().add(1, 'days').hours(0).minutes(0).seconds(0)-moment())
+		}, reset)
 	},    
 
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		var lastMonth = this.config.startMonth + this.config.monthCount - 1;
-		var todayNum = moment().format("D");
+  // Override dom generator.
+  getDom: function () {
+	date = new Date();
+    month = date.getMonth();
+    day = date.getDate();
+    year = date.getFullYear();
+    var wrapper = document.createElement("div");
+    var lastMonth = this.config.startMonth + this.config.monthCount - 1;
 
-		// pre-calculcate the header line containing the week days - no need to repeat this for every month
-		var weekdays = moment.weekdaysShort(true);
-		var weekdaysHeader = "<div class='days-header'>";
-		if (this.config.weekNumbers) {
-			// empty cell as a placeholder for the week number
-			weekdaysHeader += "<div class='day-header'>&nbsp;&nbsp;&nbsp;</div>";      
-		}
-		for (day = 0; day < 7; day++) {
-			weekdaysHeader += "<div class='day-header'>" + weekdays[day] + "</div>";
-		}
-		weekdaysHeader += "</div>";
+    // pre-calculcate the header line containing the week days - no need to repeat this for every month
+    var weekdays = moment.weekdaysShort(true);
+    var weekdaysHeader = "<div class='days-header'>";
+    if (this.config.weekNumbers) {
+      // empty cell as a placeholder for the week number
+      weekdaysHeader += "<div class='day-header'>&nbsp;&nbsp;&nbsp;</div>";
+    }
+    for (day = 0; day < 7; day++) {
+      weekdaysHeader += "<div class='day-header'>" + weekdays[day] + "</div>";
+    }
+    weekdaysHeader += "</div>";
 
-		// set calendar main container depending on calendar orientation 
-		if (this.config.monthsVertical) {
-			output = "<div class='calendar calendar-vertical'>";
-		} else {
-			output = "<div class='calendar calendar-horizontal'>";
-		}
+    // set calendar main container depending on calendar orientation
+    if (this.config.monthsVertical) {
+      output = "<div class='calendar calendar-vertical'>";
+    } else {
+      output = "<div class='calendar calendar-horizontal'>";
+    }
+ // iterate through months to display
+    for (
+      currentMonth = this.config.startMonth;
+      currentMonth <= lastMonth;
+      currentMonth++
+    ) {
+      output += "<div class='month'>";
 
-		// iterate through months to display
-		for (currentMonth = this.config.startMonth; currentMonth <= lastMonth; currentMonth++) {
-			output += "<div class='month'>";
+      // add the month headers
+      monthTemp = month + currentMonth;
+      monthTitle = moment().add(currentMonth, "month").format("MMMM YYYY");
+      output += "<div class='month-header'>" + monthTitle + "</div>";
 
-			// add the month and week day headers
-			monthTitle = moment().add(currentMonth, "month").format("MMMM YYYY");
-			output += "<div class='month-header'>" + monthTitle + "</div>";
-			if ((!this.config.monthsVertical) || ((this.config.repeatWeekdaysVertical || (currentMonth == this.config.startMonth)))) {
-				output += weekdaysHeader;
-			}
+      // add day of week headers
+      if (
+        !this.config.monthsVertical ||
+        this.config.repeatWeekdaysVertical ||
+        currentMonth == this.config.startMonth
+      ) {
+        output += weekdaysHeader;
+      }
 
-			// get the start of the week that contains the first day of the month
-			firstDayOfMonth = moment().add(currentMonth, "month").startOf("month") 
-			currentWeekday = moment().add(currentMonth, "month").startOf("month").startOf("week");
+      firstDayOfMonth = new Date(year, month + currentMonth, 1, 0, 0, 0, 0);
+      gridDay = moment().add(currentMonth, "month").startOf("month").startOf("week");
+      gridEnd = moment().add(currentMonth, "month").endOf("month").endOf("week");
 
-			// get the end of the week that contains the last day of the month
-			lastWeekday = moment().add(currentMonth, "month").endOf("month").endOf("week");
+      // Week grid builder
+      do {
+        output += "<div class='week'>";
+        if (this.config.weekNumbers) {
+          output += "<div class='weeknumber'>" + gridDay.format("W") + "</div>";
+        }
+        for (dow = 0; dow <= 6; dow++) {
+          if (gridDay.isSame(firstDayOfMonth, "month")) {
+            if (gridDay.isSame(moment(), "day")) {
+              if (this.config.highlightWeekend) {
+                if (gridDay.day() == 0 || gridDay.day() == 6) {
+                  output +=
+                    "<div class='current_day_weekend'>" +
+                    gridDay.format("D") +
+                    "</div>";
+                } else {
+                  output +=
+                    "<div class='current_day'>" +
+                    gridDay.format("D") +
+                    "</div>";
+                }
+              } else {
+                output +=
+                  "<div class='current_day'>" + gridDay.format("D") + "</div>";
+              }
+            } else {
+              if (this.config.highlightWeekend) {
+                if (gridDay.day() == 0 || gridDay.day() == 6) {
+                  output +=
+                    "<div class='weekend'>" + gridDay.format("D") + "</div>";
+                } else {
+                  output +=
+                    "<div class='day'>" + gridDay.format("D") + "</div>";
+                }
+              } else {
+                output += "<div class='day'>" + gridDay.format("D") + "</div>";
+              }
+            }
+          } else {
+            // empty cell as placeholder
+            if (this.config.monthCount == 1) {
+              output += "<div class='daydim'>" + gridDay.format("D") + "</div>";
+            } else {
+              output += "<div class='daydim'>&nbsp;</div>";
+            }
+          }
+          gridDay.add(1, "days");
+        }
+        output += "</div>"; // end of week
+      } while (gridDay.isSameOrBefore(gridEnd, "day"));
 
-			do {
-				output += "<div class='week'>";
-				if (this.config.weekNumbers) {
-					output += "<div class='weeknumber'>" + currentWeekday.format("W") + "</div>";
-				}
-				for (dow = 0; dow <= 6; dow++) {
-					if (currentWeekday.isSame(firstDayOfMonth, "month")) {
-						if (currentWeekday.isSame(moment(), "day")) {
-							if (this.config.highlightWeekend) {
-								if (currentWeekday.day() == 0 || currentWeekday.day() == 6) {
-									output += "<div class='current_day_weekend'>" + currentWeekday.format("D") + "</div>";
-								} else {
-									output += "<div class='current_day'>" + currentWeekday.format("D") + "</div>";
-								}
-							} else {
-								output += "<div class='current_day'>" + currentWeekday.format("D") + "</div>";
-							}
-						} else { // End of current day 
-							if (this.config.highlightWeekend) {
-								if (currentWeekday.day() == 0 || currentWeekday.day() == 6) {
-									output += "<div class='weekend'>" + currentWeekday.format("D") + "</div>";
-								} else {
-									output += "<div class='day'>" + currentWeekday.format("D") + "</div>";
-								}
-							} else {
-								output += "<div class='day'>" + currentWeekday.format("D") + "</div>";
-							}
-						}
-					} else {
-						// empty cell as placeholder
-						if (this.config.monthCount == 1) {
-				output += "<div class='daydim'>"+currentWeekday.format("D")+"</div>";
-			} else {
-				output += "<div class='daydim'>&nbsp;</div>";
-			}
-					}
-					currentWeekday.add(1, "days");
-				}
-				output += "</div>"; // end of week
-			} while (currentWeekday.isSameOrBefore(lastWeekday, "day"));
+      output += "</div>"; // end of month
+    }
 
-			output += "</div>"; // end of month
-		}
+    output += "</div>"; // end of calendar
 
-		output += "</div>"; // end of calendar
-
-		wrapper.innerHTML = output;
-		return wrapper;
-	}
+    wrapper.innerHTML = output;
+    return wrapper;
+  }
 });
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ### NOTE: THIS MODULE IS TO BE HOSTED ONLY AT https://github.com/BKeyport/MMM-Multimonth - Any other hosting location is invalid.
 
-If you fork and change any code, please let me know, if it's useful for the general public, I'd love to add it! 
+If you fork and change any code, please let me know, if it's useful for the general public, I'd love to add it!
 
 If you find this module useful, and would like to contribute to the project, I appreciate the thought. Instead of giving to me, please donate to L'Arche Tahoma Hope
 at https://www.larchetahomahope.org/donate/ - in honor of Nancy Tyson. (Dedicate my donation checkbox). Nancy means the world to me. You don't have to notify me you
-have done so. 
+have done so.
 
-THANK YOU for your consideration. 
+THANK YOU for your consideration.
 
 # MagicMirror Module to display a calendar with multiple months (user configurable)
 
-*** This module may break in future versions of MagicMirror. Working on a solution *** 
+**_ This module may break in future versions of MagicMirror. Working on a solution _**
 
-This is a module for the [MagicMirror](https://github.com/MichMich/MagicMirror) project. 
+This is a module for the [MagicMirror](https://github.com/MichMich/MagicMirror) project.
 
-It's purpose is to enable a mini-calendar with as many months as the user wants, assuming screen space is available. Screenshots are with config examples. 
+It's purpose is to enable a mini-calendar with as many months as the user wants, assuming screen space is available. Screenshots are with config examples.
 
 ## Installation
 
@@ -23,16 +23,17 @@ Clone this repository in your ~/MagicMirror/modules/ folder ( $ cd ~MagicMirror/
 git clone https://github.com/BKeyport/MMM-Multimonth.git
 
 ## Using the module
+
 To use this module, add it to the modules array in the config/config.js file:
 
 ```
 {
   module: 'MMM-Multimonth',
 	position: 'top_left', // can be any of the postions
-	config: { // Optional - will default to 3 months, with one previous and one next, vertical orientation. 
-		startMonth: -1, // Define when you start from current month (negative is before current, zero is current, positive is in future) 
-		monthCount: 3, //  How many months to display - If Month Count is 1, Calendar will show previous and next month dates in empty spots.  
-		// See screenshots for examples of the following config items. 
+	config: { // Optional - will default to 3 months, with one previous and one next, vertical orientation.
+		startMonth: -1, // Define when you start from current month (negative is before current, zero is current, positive is in future)
+		monthCount: 3, //  How many months to display - If Month Count is 1, Calendar will show previous and next month dates in empty spots.
+		// See screenshots for examples of the following config items.
 		monthsVertical: true, // Whether to arrange the months vertically (true) or horizontally (false).
 		repeatWeekdaysVertical: false, // Whether to repeat the week days in each month in vertical mode. Ignored in horizontal mode.
 		weekNumbers: false, // Whether to display the week numbers in front of each week.
@@ -41,19 +42,18 @@ To use this module, add it to the modules array in the config/config.js file:
 },
 ```
 
-| Screenshot | How to get |
-| --- | --- |
-| ![screenshot](vert-noweek-norep.png?raw=true "Screenshot (vertical mode, no week numbers, single weekday line)") | monthsVertical: true, <br> 		repeatWeekdaysVertical: false, <br> weekNumbers: false, |
-| ![screenshot](vert-noweek-rep.png?raw=true "Screenshot (vertical mode, no week numbers, repeat weekday line for every month)")| monthsVertical: true, <br> 		repeatWeekdaysVertical: true, <br> weekNumbers: false,  |
-| ![screenshot](vert-week-norep.png?raw=true "Screenshot (vertical mode, no week numbers, single weekday line)") | monthsVertical: true, <br> repeatWeekdaysVertical: false, <br> weekNumbers: true, |
-| ![screenshot](vert-week-rep.png?raw=true "Screenshot (vertical mode, no week numbers, repeat weekday line for every month)") | monthsVertical: true, <br> 		repeatWeekdaysVertical: true, <br> weekNumbers: true, |
-| ![screenshot](horz-noweeknum.png?raw=true "Screenshot (horizontal mode)")| monthsVertical: false, <br> weekNumbers: false, |
-| ![screenshot](horz-week.png?raw=true "Screenshot (horizontal mode)") | monthsVertical: false, <br> weekNumbers: true, |
+| Screenshot                                                                                                                     | How to get                                                                         |
+| ------------------------------------------------------------------------------------------------------------------------------ | ---------------------------------------------------------------------------------- |
+| ![screenshot](vert-noweek-norep.png?raw=true "Screenshot (vertical mode, no week numbers, single weekday line)")               | monthsVertical: true, <br> repeatWeekdaysVertical: false, <br> weekNumbers: false, |
+| ![screenshot](vert-noweek-rep.png?raw=true "Screenshot (vertical mode, no week numbers, repeat weekday line for every month)") | monthsVertical: true, <br> repeatWeekdaysVertical: true, <br> weekNumbers: false,  |
+| ![screenshot](vert-week-norep.png?raw=true "Screenshot (vertical mode, no week numbers, single weekday line)")                 | monthsVertical: true, <br> repeatWeekdaysVertical: false, <br> weekNumbers: true,  |
+| ![screenshot](vert-week-rep.png?raw=true "Screenshot (vertical mode, no week numbers, repeat weekday line for every month)")   | monthsVertical: true, <br> repeatWeekdaysVertical: true, <br> weekNumbers: true,   |
+| ![screenshot](horz-noweeknum.png?raw=true "Screenshot (horizontal mode)")                                                      | monthsVertical: false, <br> weekNumbers: false,                                    |
+| ![screenshot](horz-week.png?raw=true "Screenshot (horizontal mode)")                                                           | monthsVertical: false, <br> weekNumbers: true,                                     |
 
+Many thanks to kirash for the inspiration with the monthly_calendar module, sdetweil and lavolp3 for the help in coding and CSS work. Without your help, this would just be a pipe dream.
 
-Many thanks to kirash for the inspiration with the monthly_calendar module, sdetweil and lavolp3 for the help in coding and CSS work. Without your help, this would just be a pipe dream. 
-
-This is a basic module, but I felt it was good enough for release to the public. Please, if you have any suggestions for improvement, let me know, I'm learning JS and CSS as I write this, and I dream of much bigger things. 
+This is a basic module, but I felt it was good enough for release to the public. Please, if you have any suggestions for improvement, let me know, I'm learning JS and CSS as I write this, and I dream of much bigger things.
 
 Localization is provided by moment.js and controlled by the master language of MagicMirror. Please file any errors with localizations as bugs with the moment.js team - https://github.com/moment/moment/
 
@@ -64,31 +64,31 @@ Localization is provided by moment.js and controlled by the master language of M
 ― Voltaire
 
 ## Versioning
+
 0.1 - Initial Release
 
 0.2 - Change code to improve functionality, allow for some localization - will not move start of week yet, but will change languages with system.
 
-0.25 - Fix code with temporary bodge so to refresh every hour to assure day changes sometime between midnight and 1 am. 
+0.25 - Fix code with temporary bodge so to refresh every hour to assure day changes sometime between midnight and 1 am.
 
-0.3 - Removed bodge in 0.25, replaced with a simple refresh at midnight, no animation, as I find it annoying. 
+0.3 - Removed bodge in 0.25, replaced with a simple refresh at midnight, no animation, as I find it annoying.
 
-0.9 - Start of week now moves per moment.js. Note: at this point, module is almost everything I wanted. If I don't get any bug reports that I can control, I'm going to bump it up to 1.0 and call it good. 
+0.9 - Start of week now moves per moment.js. Note: at this point, module is almost everything I wanted. If I don't get any bug reports that I can control, I'm going to bump it up to 1.0 and call it good.
 
-0.95 - Fix start of week issue. Had a calculation backwards. - Suggestion was made to optionally add week numbers to the system, still working on that. 
+0.95 - Fix start of week issue. Had a calculation backwards. - Suggestion was made to optionally add week numbers to the system, still working on that.
 
-0.99 - Major rewrite by [Volker Wegert](https://github.com/vwegert) (Danke sehr!) - Implements everything I want, and I even understand the wizard level code he wrote. 
+0.99 - Major rewrite by [Volker Wegert](https://github.com/vwegert) (Danke sehr!) - Implements everything I want, and I even understand the wizard level code he wrote.
 
-1.00 - Minor changes to code/css to standardize look.  
+1.00 - Minor changes to code/css to standardize look.
 
-1.10 - Added leading and trailing months when 1 month is selected per user request. 
+1.10 - Added leading and trailing months when 1 month is selected per user request.
 
-1.20 - Added weekend highlight, default is off. 
+1.20 - Added weekend highlight, default is off.
 
-1.21 - Added weekend highlight for current day, so user could specify a different look for current day on weekend. 
+1.21 - Added weekend highlight for current day, so user could specify a different look for current day on weekend.
 
 1.50 - Minor fixes
 
-1.90 - Minor fixes/Remove EOL - Begin work on moving to a life without moment.js - Goal, no dependencies. 
+1.90 - Minor fixes/Remove EOL - Begin work on moving to a life without moment.js - Goal, no dependencies.
 
-7 Sept. 2022 - changing to date based versioning. 
-
+7 Sept. 2022 - changing to date based versioning.
